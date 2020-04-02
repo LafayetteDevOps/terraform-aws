f9b384045bf8592ec7f83ef7358c586af5168d79
Chris <Chris.Bachmeyer@gmail.com>
2020-04-02 15:04:59 -0600

    adjusted aws_key_pair block, corrected attribute reference in aws_instance block

--- instance/terraform/main.tf
+++ instance/terraform/main.tf
@@ -7,6 +7,7 @@ resource "aws_key_pair" "key" {
 resource "aws_instance" "single_instance" {
   ami 		= "ami-0fc61db8544a617ed"
   instance_type	= "t2.micro"
+  key_name	= aws_key_pair.key.key_name
 
   tags = { 
     Name = "t2.micro instance"
