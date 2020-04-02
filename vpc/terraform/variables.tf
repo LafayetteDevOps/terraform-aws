f67f3c739d97dffe28bf4edcbaf09df20b288163
Chris <Chris.Bachmeyer@gmail.com>
2020-04-01 19:53:51 -0600

    adding variables

---
+++ vpc/terraform/variables.tf
@@ -0,0 +1,24 @@
+// IaC deployment variables
+
+variable "aws_region" {
+  type          = string
+  default       = "us-east-1"
+}
+
+variable "type" {
+  description = "instance type"
+  type = string
+  default = "t2.micro"
+}
+
+variable "vpc_cidr" {
+  description   = "cidr block"
+  type          = string
+  default       = " "
+}
+
+variable "vpc_id" {
+  description   = "vpc_id"
+  string        = string
+  default       = " "
+}
