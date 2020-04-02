7030b6384c27e5fdf25286ac3daded6215ab3cd5
Chris <Chris.Bachmeyer@gmail.com>
2020-04-02 15:54:06 -0600

    added aws_key_pair, attribute reference, and count.index to modify instance names

--- instances/terraform/main.tf
+++ instances/terraform/main.tf
@@ -1,11 +1,16 @@
 // IaC main.tf
 
-resource "aws_instance" "single_instance" {
+resource "aws_key_pair" "key" {
+  public_key = file("~/.ssh/aws_key.pub")
+}
+
+resource "aws_instance" "multiple" {
   ami           = "ami-0fc61db8544a617ed"
   instance_type = var.type
+  key_name	= aws_key_pair.key.key_name
   count =2
 
   tags = {
-    Name = "t2.micro instance"
+    Name = "Terraform t2.micro instance-${count.index +1}"
   }
 }
