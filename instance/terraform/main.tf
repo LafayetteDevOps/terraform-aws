162f518e4fed62d25a331358b392e70f9cbd2920
Chris <Chris.Bachmeyer@gmail.com>
2020-04-02 14:01:00 -0600

    added aws_key_pair block

--- instance/terraform/main.tf
+++ instance/terraform/main.tf
@@ -1,5 +1,9 @@
 // IaC main.tf
 
+resource "aws_key_pair" "key" {
+  public_key = file("~/.ssh/aws_key.pub")
+}
+
 resource "aws_instance" "single_instance" {
   ami 		= "ami-0fc61db8544a617ed"
   instance_type	= "t2.micro"
