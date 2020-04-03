4b6926ea199388027f31f9a1eac640d613759593
Christopher Bachmeyer <Chris.Bachmeyer@gmail.com>
2020-04-03 13:39:44 -0600

    Creating vpc.tf with content

---
+++ vpc/terraform/vpc.tf
@@ -0,0 +1,67 @@
+// vpc details
+data "aws_vpc" "main" {
+  cidr_block	= var.vpc_cidr
+
+  tags = {
+    Name	= "PLM test vpc"
+    Environment	= var.environment
+    Contact	= var.contact 
+  }
+}
+
+// Networking
+resource "aws_subnet" "main" {
+  vpc_id	= var.vpc_id
+  cidr_block	= var.vpc_cidr
+
+  tags = {
+    Name = "PLM main subnet - var.environment"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "aws_vpc.default.id"
+
+  tags = {
+    name = "PLM test deploy net gw  - var.environment"
+  }
+}
+
+resource "aws_default_route_table" "r" {
+  default_route_table_id = "aws_vpc.main.default_route_table_id"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "aws_internet_gateway.gw.id"
+  }
+  tags = {
+    Name = "default table"
+  }
+}
+
+resource "aws_security_group"  "default" {
+  name		= "PLM Sec Grp"
+  description	= "Security group for PLM test deployment"
+  vpc_id	= "aws_vpc.main.id"
+
+  ingress {
+    from_port 	= 22
+    to_port 	= 22
+    protocol 	= "tcp"
+    cidr_blocks = ["10.173.44.0/22"]
+  }
+
+  ingress {
+    from_port 	= 80
+    to_port 	= 80
+    protocol 	= "tcp"
+    cidr_blocks = ["10.173.44.0/22"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
