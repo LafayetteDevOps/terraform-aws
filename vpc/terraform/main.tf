990c592040d0f0e1a7e24e8f883fafa282e8deb9
Chris <Chris.Bachmeyer@gmail.com>
2020-04-02 20:30:04 -0600

    added two more instances with varying machine types; adjusted tags accordingly

--- vpc/terraform/main.tf
+++ vpc/terraform/main.tf
@@ -5,7 +5,18 @@ resource "aws_key_pair" "key" {
   public_key = file("~/.ssh/aws_key.pub")
 }
 
-resource "aws_instance" "ec2_instance" {
+resource "aws_instance" "ec2_nano_instance" {
+  ami           = var.al2_ami_id
+  instance_type = "t2.nano"
+  count = 1
+  key_name      = aws_key_pair.key.key_name
+
+  tags = {
+    Name = "t2.nano instance-${count.index +1}"
+  }
+}
+
+resource "aws_instance" "ec2_micro_instance" {
   ami 		= var.al2_ami_id
   instance_type	= var.type
   count = 1
@@ -13,5 +24,16 @@ resource "aws_instance" "ec2_instance" {
 
   tags = {
     Name = "t2.micro instance-${count.index +1}"
+  }
+}
+
+resource "aws_instance" "ec2_small_instance" {
+  ami           = var.al2_ami_id
+  instance_type = "t2.small"
+  count = 1
+  key_name      = aws_key_pair.key.key_name
+
+  tags = {
+    Name = "t2.small instance-${count.index +1}"
   }
 }
