0b5cdb9fe84db1caa3030a5d0951f86744789b51
Chris <Chris.Bachmeyer@gmail.com>
2020-04-04 14:08:49 -0600

    added subnet_id argument to each instance

--- vpc/terraform/main.tf
+++ vpc/terraform/main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "ec2_nano_instance" {
   ami           = var.al2_ami_id
   instance_type = "t2.nano"
   count = 1
+  subnet_id	= aws_subnet.main.id
   key_name      = aws_key_pair.key.key_name
 
   tags = {
@@ -20,6 +21,7 @@ resource "aws_instance" "ec2_micro_instance" {
   ami 		= var.al2_ami_id
   instance_type	= var.type
   count = 1
+  subnet_id     = aws_subnet.main.id
   key_name	= aws_key_pair.key.key_name
 
   tags = {
@@ -31,6 +33,7 @@ resource "aws_instance" "ec2_small_instance" {
   ami           = var.al2_ami_id
   instance_type = "t2.small"
   count = 1
+  subnet_id        = aws_subnet.main.id
   key_name      = aws_key_pair.key.key_name
 
   tags = {
